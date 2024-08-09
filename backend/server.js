b6c9ce0f6f461d4200a1da83eb3d324145c1c464
rehabkhaleel <rehabkhaleel716@gmail.com>
2024-08-09 16:01:28 +0500

    For Deployment.

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+// For deployement
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
@@ -12,6 +13,7 @@ import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
+// For Deployment
 const __dirname = path.resolve();
 // PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
 const PORT = process.env.PORT || 5000;
@@ -23,6 +25,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+// For Deployment
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
