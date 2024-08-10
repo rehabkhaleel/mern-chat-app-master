9638ac8232f4d97a69ca75c661b76610fe336550
rehabkhaleel <rehabkhaleel716@gmail.com>
2024-08-10 23:16:22 +0500

    Footer Centered.

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -6,15 +6,16 @@ import Sidebar from "../../components/sidebar/Sidebar";
 const Home = () => {
   return (
     <div className="flex flex-col h-screen overflow-scroll min-w-[320px] max-w-[1200px] bg-white shadow-slate-500 border-4 text-black">
-      <Header></Header>
-      <div className="flex h-full overflow-auto">
+      <Header />
+      <div className="flex h-full overflow-auto flex-grow">
         <Sidebar className="w-1/3 text-black" />
         <div className="w-2/3 overflow-y-auto flex flex-col">
           <MessageContainer className="flex-1 text-black" />
         </div>
-        <Footer></Footer>
       </div>
-      
+      <footer className="flex justify-center items-center w-full py-4 bg-gray-200 text-black">
+        <Footer />
+      </footer>
     </div>
   );
 };
